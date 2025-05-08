439485b358bb403ffbddf23f8491b9e8369eda6b
Higor S. <stanleyhigor1@gmail.com>
2025-05-08 11:41:38 -0300

    packages and theming first try implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,40 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'app-crud';
+
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleTheme() {
+    const currentTheme = document.documentElement.classList.contains('dark-theme');
+
+    if (currentTheme) {
+      this.setLightTheme();
+    } else {
+      this.setDarkTheme();
+    }
+  }
+
+  // Apply light theme
+  setLightTheme() {
+    this.renderer.removeClass(document.documentElement, 'dark-theme');
+    this.renderer.addClass(document.documentElement, 'light-theme');
+    this.changeStylesheet('light-theme');
+  }
+
+  // Apply dark theme
+  setDarkTheme() {
+    this.renderer.removeClass(document.documentElement, 'light-theme');
+    this.renderer.addClass(document.documentElement, 'dark-theme');
+    this.changeStylesheet('dark-theme');
+  }
+
+  // Change stylesheet dynamically
+  changeStylesheet(theme: string) {
+    const themeLink = document.getElementById('theme-stylesheet') as HTMLLinkElement;
+
+    if (themeLink) {
+      themeLink.href = `${theme}.css`;
+    }
+  }
 }
